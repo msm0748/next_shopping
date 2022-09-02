8e5d69725ab4dedc52c8756d3bedcb8dc7432d2a
Moon-Seokmin <msm0748@gmail.com>
2022-09-02 20:45:43 +0900

    dd

--- pages/login.jsx
+++ pages/login.jsx
@@ -7,72 +7,58 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 
 export default function LoginPage() {
-    const router = useRouter();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const onChange = (e) => {
-        const {
-            target: { name, value },
-        } = e;
-        switch (name) {
-            case "email":
-                return setEmail(value);
-            case "password":
-                return setPassword(value);
-            default:
-                return;
-        }
-    };
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "email":
+        return setEmail(value);
+      case "password":
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        await signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                router.push("/");
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode);
-            });
-    };
-    return (
-        <Form onSubmit={onSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    name="email"
-                    value={email}
-                    onChange={onChange}
-                />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        router.push("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+      });
+  };
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" name="email" value={email} onChange={onChange} />
+        <Form.Text className="text-muted">이메일 입력</Form.Text>
+      </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={onChange}
-                />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                로그인
-            </Button>
-            <Link href="/signup">
-                <a>회원가입</a>
-            </Link>
-        </Form>
-    );
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" name="password" value={password} onChange={onChange} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        로그인
+      </Button>
+      <Link href="/signup">
+        <a>회원가입</a>
+      </Link>
+    </Form>
+  );
 }
